1957e80129917b78fc35c0217b2d8ee68d6ce84b
Tatyana905 <905478@mail.ru>
2022-08-19 11:09:51 +0300

    Zadacha3

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//  Программа, к-рая на вход принимает число N, а на выходе показывает 
+// все целые числа в  промежутке
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> "-2, -1, 0, 1, 2"
+
+int number = Convert.ToInt32(Console.ReadLine());
+// -N = N * (-1)
+int negativNumber = number * (-1);
+
+while (negativNumber <= number)
+{
+    Console.WriteLine(negativNumber);
+    negativNumber++; // 
+}
+
+
+
+Console.WriteLine("Hello, World!");
